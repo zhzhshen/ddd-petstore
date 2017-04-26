615c94eda591689a9555f9dc204a321361aabffb
林帆 <flin@thoughtworks.com>
2017-04-26 17:29:12 +0800

    remove user domain

--- src/main/java/com/cutepet/repositories/User/PetInCartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cutepet.repositories.User;
-
-import com.cutepet.domain.User.PetInCart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PetInCartRepository extends JpaRepository<PetInCart, Long> {
-
-}
