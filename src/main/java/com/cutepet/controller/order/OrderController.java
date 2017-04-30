6428a2fead677388bd9ae964c10670dbd2382e76
Lin Fan <flin@thoughtworks.com>
2017-04-30 10:30:19 +0800

    add domain layer

--- src/main/java/com/cutepet/controller/order/OrderController.java
+++ src/main/java/com/cutepet/controller/order/OrderController.java
@@ -1,13 +1,9 @@
 package com.cutepet.controller.order;
 
+import com.cutepet.domain.order.Order;
 import com.cutepet.persistence.common.PaymentMethod;
 import com.cutepet.persistence.common.PetType;
-import com.cutepet.persistence.common.Utils;
-import com.cutepet.persistence.entity.order.CustomerEntity;
-import com.cutepet.persistence.entity.order.OrderEntity;
-import com.cutepet.persistence.entity.order.PetEntity;
-import com.cutepet.persistence.repositories.order.OrderRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.google.common.collect.ImmutableMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -16,28 +12,25 @@ import java.util.*;
 @RequestMapping("/orders")
 public class OrderController {
 
-    @Autowired
-    OrderRepository orderRepository;
-
     @RequestMapping(value = "", method = RequestMethod.POST)
     public Map<String, Object> createOrder(@RequestBody Map<String, Object> body) {
         Map<String, Object> ret = new HashMap<>();
 
-        Map<String, Object> orderData = (Map)body.get("data");
-        Long userId = (Long) orderData.get("userId");
+        Map<String, Object> orderData = (Map) body.get("data");
+        Map<String, String> customerMap = (Map) orderData.get("customer");
+        Order order = Order.createOrder((Long) orderData.get("userId"),
+                customerMap.get("name"), customerMap.get("phoneNum"));
 
-        List<PetEntity> pets = new ArrayList<>();
-        List<Map<String, String> > petValues = (List) orderData.get("pets");
+        List<Map<String, String>> petValues = (List) orderData.get("pets");
         petValues.forEach((petMap) ->
-                pets.add(new PetEntity(petMap.get("name"),
+                order.addPet(petMap.get("name"),
                         petMap.get("color"),
                         PetType.valueOf(petMap.get("type")),
-                        PaymentMethod.valueOf(petMap.get("payment"))))
+                        PaymentMethod.valueOf(petMap.get("payment")))
         );
-        Map<String, String> petLoverMap = (Map) orderData.get("customer");
-        CustomerEntity customer = new CustomerEntity(petLoverMap.get("name"), petLoverMap.get("phoneNum"));
-        orderRepository.save(new OrderEntity(new Date(), userId, pets, customer));
+        order.save();
 
+        ret.put("data", ImmutableMap.of("status", "OK"));
         return ret;
     }
 
@@ -45,18 +38,7 @@ public class OrderController {
     public Map<String, Object> getAllOrders() {
         Map<String, Object> ret = new HashMap<>();
 
-        List<Map<String, Object>> orderList = new ArrayList<>();
-        orderRepository.findAll().forEach((order -> {
-            try {
-                List<PetEntity> pets = order.getPets();
-                Map<String, Object> orderMap = Utils.introspect(order);
-                orderMap.put("pets", pets);
-                orderList.add(orderMap);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }));
-        ret.put("data", orderList);
+        ret.put("data", Order.getAllOrders());
 
         return ret;
     }
@@ -65,16 +47,7 @@ public class OrderController {
     public Map<String, Object> getOrder(@PathVariable long order_id) {
         Map<String, Object> ret = new HashMap<>();
 
-        OrderEntity order = orderRepository.findById(order_id).get(0);
-        List<PetEntity> pets = order.getPets();
-        Map<String, Object> orderMap = null;
-        try {
-            orderMap = Utils.introspect(orderRepository.findById(order_id).get(0));
-            orderMap.put("pets", pets);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        ret.put("data", orderMap);
+        ret.put("data", Order.getOrder(order_id));
 
         return ret;
     }
--- src/main/java/com/cutepet/domain/order/Order.java
+++ src/main/java/com/cutepet/domain/order/Order.java
@@ -1,4 +1,68 @@
 package com.cutepet.domain.order;
 
+import com.cutepet.persistence.common.PaymentMethod;
+import com.cutepet.persistence.common.PetType;
+import com.cutepet.persistence.entity.order.CustomerEntity;
+import com.cutepet.persistence.entity.order.OrderEntity;
+import com.cutepet.persistence.entity.order.PetEntity;
+import com.cutepet.persistence.repositories.order.OrderRepository;
+import com.cutepet.utils.ContextHolder;
+import com.cutepet.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.*;
+
 public class Order {
+
+    @Autowired
+    OrderRepository orderRepository;
+    private Long userId;
+    private List<PetEntity> pets;
+    private CustomerEntity customer;
+
+    public Order() {
+    }
+
+    public void addPet(String name, String color, PetType type, PaymentMethod payment) {
+        pets.add(new PetEntity(name, color, type, payment));
+    }
+
+    public void save() {
+        orderRepository.save(new OrderEntity(new Date(), userId, pets, customer));
+    }
+
+    public static Order createOrder(Long userId, String name, String phoneNum) {
+        Order order = new Order();
+        order.userId = userId;
+        order.customer = new CustomerEntity(name, phoneNum);
+        order.pets = new ArrayList<>();
+        return order;
+    }
+
+    public static List<Map<String, Object>> getAllOrders() {
+        List<Map<String, Object>> orderList = new ArrayList<>();
+        List<OrderEntity> orders = ContextHolder.getContext().getBean(OrderRepository.class).findAll();
+        orders.forEach((order -> {
+            List<PetEntity> pets = order.getPets();
+            Map<String, Object> orderMap = Utils.introspect(order);
+            orderMap.put("pets", pets);
+            orderList.add(orderMap);
+        }));
+        return orderList;
+    }
+
+    public static Map<String, Object> getOrder(long order_id) {
+        Map<String, Object> orderMap = null;
+        try {
+            OrderEntity order = ContextHolder.getContext()
+                    .getBean(OrderRepository.class).findById(order_id).get(0);
+            List<PetEntity> pets = order.getPets();
+                orderMap = Utils.introspect(order);
+                orderMap.put("pets", pets);
+        } catch (IndexOutOfBoundsException e) {
+            orderMap = new HashMap<>();
+        }
+        return orderMap;
+    }
+
 }
