fd59729e7f1110adbc66aa5b59a3cca883ebb927
Zhangzhe Shen <zzshen@thoughtworks.com>
2017-05-08 15:39:16 +0800

    fix autowire failure create multiple modules and move everything into module provider

--- provider/src/main/java/com/cutepet/domain/order/Order.java
+++ provider/src/main/java/com/cutepet/domain/order/Order.java
@@ -3,9 +3,11 @@ package com.cutepet.domain.order;
 import com.cutepet.persistence.entity.order.OrderEntity;
 import com.cutepet.persistence.repositories.order.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class Order {
 
     @Autowired
