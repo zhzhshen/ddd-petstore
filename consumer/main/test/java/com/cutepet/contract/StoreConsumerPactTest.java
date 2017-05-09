2f76fbb0e542643eaa894970da9c4fd987e63a8d
Zhangzhe Shen <zzshen@thoughtworks.com>
2017-05-09 16:25:46 +0800

    use type matching instead of exact value matching to make pact verify pass

--- consumer/main/test/java/com/cutepet/contract/StoreConsumerPactTest.java
+++ consumer/main/test/java/com/cutepet/contract/StoreConsumerPactTest.java
@@ -3,6 +3,7 @@ package com.cutepet.contract;
 import au.com.dius.pact.consumer.Pact;
 import au.com.dius.pact.consumer.PactProviderRule;
 import au.com.dius.pact.consumer.PactVerification;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.model.PactFragment;
 import com.google.common.collect.ImmutableMap;
@@ -11,7 +12,7 @@ import org.junit.Test;
 
 import static io.restassured.RestAssured.get;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
 
 public class StoreConsumerPactTest {
     @Rule
@@ -27,7 +28,12 @@ public class StoreConsumerPactTest {
                 .willRespondWith()
                 .headers(ImmutableMap.of("Content-Type", "application/json"))
                 .status(200)
-                .body("{\"data\":[{\"id\":1,\"name\":\"Doggy\"},{\"id\":2,\"name\":\"Catty\"}]}")
+                .body(new PactDslJsonBody()
+                        .eachLike("data")
+                            .integerType("id", 1)
+                            .stringType("name", "Doggy")
+                            .closeObject()
+                        .closeArray())
 
                 .uponReceiving("get all pets in a store")
                 .path("/stores/1/pets")
@@ -35,7 +41,14 @@ public class StoreConsumerPactTest {
                 .willRespondWith()
                 .headers(ImmutableMap.of("Content-Type", "application/json"))
                 .status(200)
-                .body("{\"data\":[{\"id\":1,\"name\":\"Dog1\",\"color\":\"black\",\"type\":\"husky\"},{\"id\":2,\"name\":\"Dog2\",\"color\":\"yellow\",\"type\":\"retriever\"}]}")
+                .body(new PactDslJsonBody()
+                        .eachLike("data")
+                            .integerType("id", 1)
+                            .stringType("name", "Dog1")
+                            .stringType("color", "black")
+                            .stringType("type", "husky")
+                            .closeObject()
+                        .closeArray())
 
                 .uponReceiving("get info of a pet in a store")
                 .path("/stores/1/pets/1")
@@ -43,19 +56,25 @@ public class StoreConsumerPactTest {
                 .willRespondWith()
                 .headers(ImmutableMap.of("Content-Type", "application/json"))
                 .status(200)
-                .body("{\"data\":{\"id\":1,\"name\":\"Dog1\",\"color\":\"black\",\"type\":\"husky\"}}")
+                .body(new PactDslJsonBody()
+                        .object("data")
+                        .integerType("id", 1)
+                        .stringType("name", "Dog1")
+                        .stringType("color", "black")
+                        .stringType("type", "husky")
+                        .closeObject())
                 .toFragment();
     }
 
     @Test
     @PactVerification
     public void storeContractTest() {
-        get("/stores").then().body("data.name", hasItems("Doggy", "Catty"));
+        get("/stores").then().body("data.name", hasItem("Doggy"));
 
         get("/stores/1/pets").then()
-                .body("data.id", hasItems(1, 2))
-                .body("data.name", hasItems("Dog1", "Dog2"))
-                .body("data.type", hasItems("husky", "retriever"));
+                .body("data.id", hasItem(1))
+                .body("data.name", hasItem("Dog1"))
+                .body("data.type", hasItem("husky"));
 
         get("/stores/1/pets/1").then()
                 .body("data.id", equalTo(1))
